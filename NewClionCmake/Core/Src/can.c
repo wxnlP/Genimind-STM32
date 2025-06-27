685ff75a959b7cbace94db7907318e98e78eddd2
lzh <2661006892@qq.com>
2025-06-27 14:57:10 +0800

    补全CAN的过滤器初始化

--- NewClionCmake/Core/Src/can.c
+++ NewClionCmake/Core/Src/can.c
@@ -21,7 +21,7 @@
 #include "can.h"
 
 /* USER CODE BEGIN 0 */
-
+CAN_FilterTypeDef canFilter;
 /* USER CODE END 0 */
 
 CAN_HandleTypeDef hcan;
@@ -29,97 +29,105 @@ CAN_HandleTypeDef hcan;
 /* CAN init function */
 void MX_CAN_Init(void)
 {
+    /* USER CODE BEGIN CAN_Init 0 */
 
-  /* USER CODE BEGIN CAN_Init 0 */
+    /* USER CODE END CAN_Init 0 */
 
-  /* USER CODE END CAN_Init 0 */
+    /* USER CODE BEGIN CAN_Init 1 */
 
-  /* USER CODE BEGIN CAN_Init 1 */
-
-  /* USER CODE END CAN_Init 1 */
-  hcan.Instance = CAN1;
-  hcan.Init.Prescaler = 12;
-  hcan.Init.Mode = CAN_MODE_NORMAL;
-  hcan.Init.SyncJumpWidth = CAN_SJW_2TQ;
-  hcan.Init.TimeSeg1 = CAN_BS1_2TQ;
-  hcan.Init.TimeSeg2 = CAN_BS2_3TQ;
-  hcan.Init.TimeTriggeredMode = DISABLE;
-  hcan.Init.AutoBusOff = ENABLE;
-  hcan.Init.AutoWakeUp = DISABLE;
-  hcan.Init.AutoRetransmission = ENABLE;
-  hcan.Init.ReceiveFifoLocked = DISABLE;
-  hcan.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN CAN_Init 2 */
-
-  /* USER CODE END CAN_Init 2 */
-
+    /* USER CODE END CAN_Init 1 */
+    hcan.Instance = CAN1;
+    hcan.Init.Prescaler = 12;
+    hcan.Init.Mode = CAN_MODE_NORMAL;
+    hcan.Init.SyncJumpWidth = CAN_SJW_2TQ;
+    hcan.Init.TimeSeg1 = CAN_BS1_2TQ;
+    hcan.Init.TimeSeg2 = CAN_BS2_3TQ;
+    hcan.Init.TimeTriggeredMode = DISABLE;
+    hcan.Init.AutoBusOff = ENABLE;
+    hcan.Init.AutoWakeUp = DISABLE;
+    hcan.Init.AutoRetransmission = ENABLE;
+    hcan.Init.ReceiveFifoLocked = DISABLE;
+    hcan.Init.TransmitFifoPriority = DISABLE;
+    if (HAL_CAN_Init(&hcan) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN CAN_Init 2 */
+    HAL_CAN_Start(&hcan);
+    canFilter.FilterBank = 0;
+    canFilter.FilterIdHigh = 0x0000;
+    canFilter.FilterIdLow = 0x0000;
+    canFilter.FilterMaskIdHigh = 0x0000;
+    canFilter.FilterMaskIdLow = 0x0000;
+    canFilter.FilterScale = CAN_FILTERSCALE_32BIT;
+    canFilter.FilterMode = CAN_FILTERMODE_IDMASK;
+    canFilter.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+    canFilter.FilterActivation = CAN_FILTER_ENABLE;
+    if (HAL_CAN_ConfigFilter(&hcan, &canFilter) != HAL_OK) {
+        Error_Handler();
+    }
+    /* USER CODE END CAN_Init 2 */
 }
 
 void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
 {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if (canHandle->Instance == CAN1)
+    {
+        /* USER CODE BEGIN CAN1_MspInit 0 */
 
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(canHandle->Instance==CAN1)
-  {
-  /* USER CODE BEGIN CAN1_MspInit 0 */
+        /* USER CODE END CAN1_MspInit 0 */
+        /* CAN1 clock enable */
+        __HAL_RCC_CAN1_CLK_ENABLE();
 
-  /* USER CODE END CAN1_MspInit 0 */
-    /* CAN1 clock enable */
-    __HAL_RCC_CAN1_CLK_ENABLE();
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        /**CAN GPIO Configuration
+        PB8     ------> CAN_RX
+        PB9     ------> CAN_TX
+        */
+        GPIO_InitStruct.Pin = GPIO_PIN_8;
+        GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**CAN GPIO Configuration
-    PB8     ------> CAN_RX
-    PB9     ------> CAN_TX
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_8;
-    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+        GPIO_InitStruct.Pin = GPIO_PIN_9;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = GPIO_PIN_9;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+        __HAL_AFIO_REMAP_CAN1_2();
 
-    __HAL_AFIO_REMAP_CAN1_2();
+        /* CAN1 interrupt Init */
+        HAL_NVIC_SetPriority(USB_LP_CAN1_RX0_IRQn, 5, 0);
+        HAL_NVIC_EnableIRQ(USB_LP_CAN1_RX0_IRQn);
+        /* USER CODE BEGIN CAN1_MspInit 1 */
 
-    /* CAN1 interrupt Init */
-    HAL_NVIC_SetPriority(USB_LP_CAN1_RX0_IRQn, 5, 0);
-    HAL_NVIC_EnableIRQ(USB_LP_CAN1_RX0_IRQn);
-  /* USER CODE BEGIN CAN1_MspInit 1 */
-
-  /* USER CODE END CAN1_MspInit 1 */
-  }
+        /* USER CODE END CAN1_MspInit 1 */
+    }
 }
 
 void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
 {
+    if (canHandle->Instance == CAN1)
+    {
+        /* USER CODE BEGIN CAN1_MspDeInit 0 */
 
-  if(canHandle->Instance==CAN1)
-  {
-  /* USER CODE BEGIN CAN1_MspDeInit 0 */
+        /* USER CODE END CAN1_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_CAN1_CLK_DISABLE();
 
-  /* USER CODE END CAN1_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_CAN1_CLK_DISABLE();
+        /**CAN GPIO Configuration
+        PB8     ------> CAN_RX
+        PB9     ------> CAN_TX
+        */
+        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8 | GPIO_PIN_9);
 
-    /**CAN GPIO Configuration
-    PB8     ------> CAN_RX
-    PB9     ------> CAN_TX
-    */
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8|GPIO_PIN_9);
+        /* CAN1 interrupt Deinit */
+        HAL_NVIC_DisableIRQ(USB_LP_CAN1_RX0_IRQn);
+        /* USER CODE BEGIN CAN1_MspDeInit 1 */
 
-    /* CAN1 interrupt Deinit */
-    HAL_NVIC_DisableIRQ(USB_LP_CAN1_RX0_IRQn);
-  /* USER CODE BEGIN CAN1_MspDeInit 1 */
-
-  /* USER CODE END CAN1_MspDeInit 1 */
-  }
+        /* USER CODE END CAN1_MspDeInit 1 */
+    }
 }
 
 /* USER CODE BEGIN 1 */
